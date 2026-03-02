4b996720b9bc49f18a790e6d8f1e1daff111cab0
Dr.Isi Idemudia <icysprings@gmail.com>
2026-03-02 10:10:19 -0500

    Add main App component with layout structure

--- demo-site/src/app.tsx
+++ demo-site/src/app.tsx
@@ -1,1 +1,17 @@
+import React from "react";
+import Shell from "./components/Shell";
+import Hero from "./components/Hero";
+import FeatureGrid from "./components/FeatureGrid";
+import DemoTabs from "./components/DemoTabs";
+import Footer from "./components/Footer";
 
+export default function App() {
+  return (
+    <Shell>
+      <Hero />
+      <FeatureGrid />
+      <DemoTabs />
+      <Footer />
+    </Shell>
+  );
+}
